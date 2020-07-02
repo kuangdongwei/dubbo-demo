6063e0ee44bba9792c582e47fafe326fbe76a989
kuangdongwei <kuangdongwei666@163.com>
2020-07-02 19:14:24 +0800

    changepol

--- luckymoney/src/test/java/com/springbootdemos/luckymoney/controller/CopyTest.java
+++ luckymoney/src/test/java/com/springbootdemos/luckymoney/controller/CopyTest.java
@@ -1,27 +1,27 @@
 package com.springbootdemos.luckymoney.controller;
 
+import com.google.gson.Gson;
+import com.springbootdemos.luckymoney.model.PersonDto;
 import org.junit.Test;
+
+import java.lang.reflect.Type;
 
 public class CopyTest {
     @Test
     public void copyTest(){
-        int a = 1;
-        int b = a;
-        System.out.println("a="+a + " b="+b);
-        a++;
-        System.out.println("a="+a + " b="+b);
+        PersonDto personDto = new PersonDto();
+        personDto.setAge("24");
+        personDto.setId("111");
+        personDto.setName("Chandler");
+        personDto.setSex("male");
 
+        int testNumber = 123456;
+        Gson gson = new Gson();
+        System.out.println("自定义对象类型序列化为字符串："+gson.toJson(personDto));
+        System.out.println("基本类型序列化为字符串："+ gson.toJson(testNumber));
 
-        Integer aa = 11;
-        Integer bb = aa;
-        System.out.println("aa=" + aa + " bb="+bb);
-        aa++;
-        System.out.println("aa=" + aa + " bb="+bb);
-
-        StringBuilder haha = new StringBuilder("hello");
-        StringBuilder xixi = haha;
-        System.out.println("haha:"+haha+" xixi:"+xixi);
-        haha.append("world");
-        System.out.println("haha:"+haha+" xixi:"+xixi);
+//        String jsonString = "{\"id\":\"222\",\"name\":\"小菜鸡\",\"sex\":\"male\",\"age\":\"24\"}";
+//        PersonDto personDto1 = gson.fromJson(jsonString,PersonDto.class);
+//        System.out.println("personDto1的名字为："+personDto1.getName());
     }
 }
